e29aa9c40f06ecc2777675d19c668b0f482471b9
Coleton Watt <electricninja102@gmail.com>
2022-01-22 12:12:52 -0700

    Initial Commit

---
+++ Lab: Range 2.4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab: Range 2.4
+//
+//  Created by Coleton Watt on 1/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
